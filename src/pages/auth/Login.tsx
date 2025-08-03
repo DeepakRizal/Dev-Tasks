4d9dacd93a6470b19e3b580a61bc3d21960e2931
Deepak Rizal <deepak2000rizal@gmail.com>
2025-08-03 16:44:01 +0530

    feat: implement validation in login form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,22 +1,71 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+interface formState {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
+  const [form, setForm] = useState<formState>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  function validateForm(data: formState) {
+    const newErros: Record<string, string> = {};
+    if (!data.email.trim()) newErros.email = "Email is required!";
+    if (!data.password.trim()) newErros.password = "Password is required!";
+
+    return newErros;
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const validationErrors = validateForm(form);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+  }
+
   return (
     <div className=" flex items-center h-[100%]  justify-center">
-      <form className="w-[350px]">
+      <form onSubmit={handleSubmit} className="w-[350px]">
         <h2 className="auth-heading-styles">Login</h2>
 
         <div className="flex flex-col gap-5 items-center justify-center mb-4">
-          <input
-            className="auth-input-styles"
-            placeholder="E-mail"
-            type="text"
-          />
-          <input
-            type="password"
-            className="auth-input-styles"
-            placeholder="Password"
-          />
+          <div className="w-full">
+            <input
+              className="auth-input-styles"
+              placeholder="E-mail"
+              name="name"
+              type="text"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setForm((prev) => ({ ...prev, email: e.target.value }))
+              }
+              value={form.email}
+            />
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
+          </div>
+
+          <div className="w-full">
+            <input
+              type="password"
+              className="auth-input-styles"
+              placeholder="Password"
+              name="password"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setForm((prev) => ({ ...prev, password: e.target.value }))
+              }
+              value={form.password}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password}</p>
+            )}
+          </div>
+
           <button
             className="py-2 font-bold text-white rounded-md w-full cursor-pointer hover:shadow-pink-500/50 transition-shadow duration-300"
             style={{
