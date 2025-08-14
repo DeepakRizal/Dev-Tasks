26c72b25bdac6f889b60008f0f124f4bd0f07bb9
Deepak Rizal <deepak2000rizal@gmail.com>
2025-08-14 19:33:43 +0530

    refactor(dashboard):move view all teams button to top and add create team button to top as well

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -29,10 +29,26 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen  bg-black text-white p-6">
       <div className="max-w-4xl mx-auto">
-        <div className="border-b border-slate-700 pb-4 mb-6">
+        <div className="border-b flex item-center justify-between border-slate-700 pb-4 mb-6">
           <h1 className="text-2xl font-bold text-white">
             Welcome, {currentUser?.name} ({currentUser?.role})
           </h1>
+
+          <div className="flex gap-5">
+            <Button
+              text="View All Teams"
+              size="sm"
+              variant="primary"
+              to="/teams"
+            />
+            <Button
+              variant="neutral"
+              onClick={handleClick}
+              text="Create Team"
+              size="sm"
+              icon={<Plus className="w-5 h-5" />}
+            />
+          </div>
         </div>
         <div className="mb-8">
           <h2 className="text-xl font-semibold text-white mb-4 border-b border-slate-700 pb-4">
@@ -61,10 +77,6 @@ const Dashboard = () => {
               icon={<Plus className="w-5 h-5" />}
             />
           )}
-        </div>
-
-        <div className="flex items-center justify-center mt-8">
-          <Button text="View all Teams" variant="primary" to="/teams" />
         </div>
       </div>
       {isOpen && <AddTeamModal isOpen={isOpen} setIsOpen={setIsOpen} />}
